d5907ca4fce543850d592ad925fff1935d6ca518
Rushin Dickson <rdickson@c3r3s5.wethinkcode.co.za>
2019-04-29 09:55:36 +0200

    first_word

--- level01/first_word.c
+++ level01/first_word.c
@@ -5,29 +5,28 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rdickson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/26 10:34:30 by rdickson          #+#    #+#             */
-/*   Updated: 2019/04/26 11:09:09 by rdickson         ###   ########.fr       */
+/*   Created: 2019/04/29 09:24:32 by rdickson          #+#    #+#             */
+/*   Updated: 2019/04/29 09:54:52 by rdickson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	ft_putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
-	return (0);
 }
 
-int	main(int ac, char **av)
+int		main(int argc, char **argv)
 {
 	int i;
 
-	if (ac == 2)
+	i = 0;
+	if (argc == 2)
 	{
-		i = 0;
-		while (av[1][i] != ' ' && av[1][i] != '\0')
+		while (argv[1][i] != ' ')
 		{
-			ft_putchar(av[1][i]);
+			ft_putchar(argv[1][i]);
 			i++;
 		}
 	}
